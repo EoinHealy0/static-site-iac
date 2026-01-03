8b20f201e72d8d96e298d595749acc07e90a96b2
Eoin Healy <119070756+EoinHealy0@users.noreply.github.com>
2026-01-03 14:12:05 +0000

    Ansible configure server (#5)
    Added 4 roles:
    configure_nginx
    configure_fail2ban
    configure_docker
    security_updates
    
    Deploys two containers to do blue/green deployments in future
    nginx configured on vm as reverse proxy for those two containers
    workflow updated to call this playbook

--- terraform/full/network.tf
+++ terraform/full/network.tf
@@ -24,6 +24,21 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+resource "azurerm_network_security_rule" "httprule" {
+  name                        = "Allow-HTTP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+  
+}
+
 resource "azurerm_subnet_network_security_group_association" "subnet_nsg_association" {
   subnet_id                 = azurerm_subnet.subnet.id
   network_security_group_id = azurerm_network_security_group.nsg.id
