094b5cae51f4e5415950566aea02a0d472aa960f
Eoin Healy <119070756+EoinHealy0@users.noreply.github.com>
2026-01-05 21:26:18 +0000

    fixed dns resources

--- terraform/full/dns.tf
+++ terraform/full/dns.tf
@@ -1,17 +1,17 @@
-resource "cloudflare_record" "dns_record" {
+resource "cloudflare_dns_record" "dns_record" {
   zone_id = var.cloudflare_zone
   name    = "@"
-  value   = azurerm_public_ip.vm_public_ip.ip_address
+  content   = azurerm_public_ip.vm_public_ip.ip_address
   type    = "A"
   ttl     = 300
   proxied = true
 }
 
-resource "cloudflare_record" "dns_record2" {
+resource "cloudflare_dns_record" "dns_record2" {
   zone_id = var.cloudflare_zone
   name    = "www"
-  value   = azurerm_public_ip.vm_public_ip.ip_address
-  type    = "A"
+  content   = azurerm_public_ip.vm_public_ip.ip_address
+  type    = "CNAME"
   ttl     = 300
   proxied = true
 }
